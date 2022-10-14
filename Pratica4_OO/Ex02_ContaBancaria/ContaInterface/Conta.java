52cfb0b923d995615fb8a01f9852cdf04f6b1be0
clesiosalmeida <69396527+clesiosalmeida@users.noreply.github.com>
2022-10-13 21:49:03 -0300

    Add files via upload

---
+++ Pratica4_OO/Ex02_ContaBancaria/ContaInterface/Conta.java
@@ -0,0 +1,8 @@
+//package Ex01_ContaBancaria.ContaInterface;
+
+public interface Conta {
+    void depositar(double valor);
+    void sacar(double valor);
+    double getSaldo();
+    
+}
---
+++ Pratica4_OO/Ex02_ContaBancaria/ContaInterface/GeradorExtratos.java
@@ -0,0 +1,8 @@
+public class GeradorExtratos {
+    public void geradorConta(Conta conta){
+        System.out.println("Saldo Atual: "+conta.getSaldo());
+    }
+}
+
+
+

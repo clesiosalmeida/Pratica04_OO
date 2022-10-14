e9d431f0dc234a70d9f423ffaa98be19a51d68b7
clesiosalmeida <69396527+clesiosalmeida@users.noreply.github.com>
2022-10-13 21:50:33 -0300

    Delete Pratica4_OO/Ex01_ContaBancaria/ContaInterface directory

--- Pratica4_OO/Ex01_ContaBancaria/ContaInterface/Conta.java
+++
@@ -1,8 +0,0 @@
-//package Ex01_ContaBancaria.ContaInterface;
-
-public interface Conta {
-    void depositar(double valor);
-    void sacar(double valor);
-    double getSaldo();
-    
-}
--- Pratica4_OO/Ex01_ContaBancaria/ContaInterface/GeradorExtratos.java
+++
@@ -1,8 +0,0 @@
-public class GeradorExtratos {
-    public void geradorConta(Conta conta){
-        System.out.println("Saldo Atual: "+conta.getSaldo());
-    }
-}
-
-
-
